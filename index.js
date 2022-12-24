518c595cd3c53d6c2ca55a84970ec497ef6d8999
ekene2416 <117322599+ekene2416@users.noreply.github.com>
2022-12-24 22:52:33 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+let username;
+ username = window.prompt('what is your name');
+ window.alert('merry christmas '+username);
+document.getElementById("wish").innerHTML = 'merry christmas ' + username;
